e6711f7b3a9738fca00d95e21a4795274e9c7cdb
mateorodriguez <MR192391@fi365.ort.edu.uy>
2024-02-09 11:15:36 -0300

    add new screen and new functions

--- Obligatorio/www/js/codigo.js
+++ Obligatorio/www/js/codigo.js
@@ -15,6 +15,7 @@ function cerrarMenu() {
 function inicio() {
   chequearSession();
   eventos();
+  obtenerPaises();
 }
 
 function eventos() {
@@ -22,6 +23,18 @@ function eventos() {
   document
     .querySelector("#btnHacerLogin")
     .addEventListener("click", previaLogin);
+  document
+    .querySelector("#btnRegistrarUsuario")
+    .addEventListener("click", previaRegistro);
+  document
+    .querySelector("#btnCerrarSesion")
+    .addEventListener("click", cerrarSesion);
+  document
+    .querySelector("#btnRegistrarC")
+    .addEventListener("click", obtenerAlimentos);
+  document
+    .querySelector("#btnRegistrarAlimento")
+    .addEventListener("click", previaRegistroAlimento);
 }
 
 function navegar(evt) {
@@ -100,18 +113,198 @@ function hacerLogin(unUsuario) {
     .then(function (data) {
       console.log(data);
       if (data.codigo === 200) {
-        alert("¡Login exitoso!");
+        document.getElementById("mensajeLoginExitoso").innerText =
+          "Login exitoso!";
         localStorage.setItem("idUsuario", data.id);
         localStorage.setItem("apikey", data.apiKey);
+        localStorage.setItem("caloriasDiarias", data.caloriasDiarias);
         ocultarPantallas();
         HOME.style.display = "block";
         MostrarMenuActivo();
+        document.querySelector("#pantalla-home ion-title").innerText =
+          "Bienvenido " + unUsuario.usuario;
+        setTimeout(function () {
+          document.getElementById("mensajeLoginExitoso").innerText = "";
+        }, 4000);
       } else {
-        alert(data.mensaje);
+        document.getElementById("mensajeError").innerText = data.mensaje;
       }
     })
     .catch(function (error) {
       console.log(error);
-      alert("Error en la solicitud al servidor");
+    });
+}
+
+function previaRegistro() {
+  let usuario = document.querySelector("#txtRegistroUsuario").value;
+  let password = document.querySelector("#txtRegistroPassword").value;
+  let idPais = document.querySelector("#txtRegistroPais").value;
+  let caloriasDiarias = document.querySelector("#txtRegistroCalorias").value;
+
+  let nuevoUsuario = new Object();
+  nuevoUsuario.usuario = usuario;
+  nuevoUsuario.password = password;
+  nuevoUsuario.idPais = idPais;
+  nuevoUsuario.caloriasDiarias = caloriasDiarias;
+
+  hacerRegistro(nuevoUsuario);
+}
+
+function hacerRegistro(nuevoUsuario) {
+  fetch(`${URLBASE}usuarios.php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(nuevoUsuario),
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.codigo === 200) {
+        document.getElementById("mensajeRegistroExitoso").innerText =
+          "Registro exitoso!";
+        document.getElementById("mensajeErrorRegistro").innerText = "";
+        limpiarCamposRegistro();
+      } else {
+        document.getElementById("mensajeErrorRegistro").innerText =
+          data.mensaje;
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function limpiarCamposRegistro() {
+  document.querySelector("#txtRegistroUsuario").value = "";
+  document.querySelector("#txtRegistroPassword").value = "";
+  document.querySelector("#txtRegistroPais").value = "";
+  document.querySelector("#txtRegistroCalorias").value = "";
+}
+
+function limpiarCamposRegistroAlimento() {
+  document.querySelector("#txtIdAlimento").value = "";
+  document.querySelector("#txtCantidadAlimento").value = "";
+  document.querySelector("#txtFechaAlimento").value = "";
+}
+
+function cerrarSesion() {
+  localStorage.clear();
+  window.location.reload();
+  ROUTER.push("/login");
+}
+
+function obtenerPaises() {
+  fetch(`${URLBASE}paises.php`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (respuesta) {
+      console.log(respuesta);
+      if (respuesta && respuesta.paises) {
+        const selectPais = document.getElementById("txtRegistroPais");
+        respuesta.paises.forEach(function (pais) {
+          const option = document.createElement("ion-select-option");
+          option.value = pais.id;
+          option.textContent = pais.name;
+          selectPais.appendChild(option);
+        });
+      } else {
+        console.error("Error al obtener los países: Datos incompletos");
+      }
+    })
+    .catch(function (error) {
+      console.error("Error al obtener los países:", error);
+    });
+}
+
+function obtenerAlimentos() {
+  apiKey = localStorage.getItem("apikey");
+  idUser = localStorage.getItem("idUsuario");
+
+  fetch(`${URLBASE}alimentos.php`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: apiKey,
+      iduser: idUser,
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (respuesta) {
+      if (respuesta && respuesta.alimentos) {
+        const selectAlimento = document.getElementById("txtIdAlimento");
+        respuesta.alimentos.forEach(function (alimento) {
+          const option = document.createElement("ion-select-option");
+          option.value = alimento.id;
+          option.textContent = alimento.nombre;
+          selectAlimento.appendChild(option);
+        });
+      } else {
+        console.error("Error al obtener los alimentos");
+      }
+    })
+    .catch(function (error) {
+      console.error("Error al obtener los alimentos:", error);
+    });
+}
+
+function previaRegistroAlimento() {
+  let idUser = localStorage.getItem("idUsuario");
+  let id = document.querySelector("#txtIdAlimento").value;
+  let cantidad = document.querySelector("#txtCantidadAlimento").value;
+  let fecha = document.querySelector("#txtFechaAlimento").value;
+
+  let nuevoAlimentoRegistrado = new Object();
+  nuevoAlimentoRegistrado.id = id;
+  nuevoAlimentoRegistrado.idUser = idUser;
+  nuevoAlimentoRegistrado.cantidad = cantidad;
+  nuevoAlimentoRegistrado.fecha = fecha;
+
+  hacerRegistroAlimento(nuevoAlimentoRegistrado);
+}
+
+function hacerRegistroAlimento(nuevoAlimentoRegistrado) {
+  apiKey = localStorage.getItem("apikey");
+  idUser = localStorage.getItem("idUsuario");
+
+  fetch(`${URLBASE}registros.php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: apiKey,
+      iduser: idUser,
+    },
+    body: JSON.stringify(nuevoAlimentoRegistrado),
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.codigo === 200) {
+        localStorage.setItem("idRegistro", data.idRegistro);
+        document.getElementById("mensajeRegistroAlimentoExitoso").innerText =
+          "Registro exitoso!";
+        document.getElementById("mensajeErrorAlimentoRegistro").innerText = "";
+        limpiarCamposRegistroAlimento();
+      } else {
+        document.getElementById("mensajeErrorAlimentoRegistro").innerText =
+          data.mensaje;
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Obligatorio_Taller_Disp_Moviles
+# Aplicación - Taller dispositivos moviles
+
+Este proyecto es una aplicación móvil diseñada para ayudar a los usuarios a llevar un registro de su consumo de alimentos y controlar su ingesta calórica diaria. La aplicación permite a los usuarios registrar los alimentos que consumen, la cantidad en la que los consumen y la fecha en que los consumen. Además, proporciona información sobre el contenido nutricional de los alimentos registrados.
+
+# Instalación de dependencias básicas para un proyecto de Ionic
+
+npm install -g @ionic/cli # Ionic CLI para crear y administrar proyectos
+
+# Información adicional
+
+Este proyecto utiliza el framework Ionic para el desarrollo de la interfaz de usuario, con JavaScript (JS) para la lógica de programación y CSS para los estilos.
+
+## Créditos
+
+- Mateo Rodriguez: https://github.com/MateoRodriguezi
+- Thiago
